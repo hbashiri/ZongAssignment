d44c640424657f8d38a125b12ad5af4f6919cdf7
Hamid Bashiri <hvbashiri@gmail.com>
2024-02-10 18:39:39 +0330

    feat: Add simple sfx to test box sounds;

---
+++ Assets/Scripts/Environment/PlayVfxAudio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.VFX;
+using UnityEngine.VFX.Utility;
+
+namespace Environment
+{
+    public class PlayVfxAudio : VFXOutputEventAbstractHandler
+    {
+        public override bool canExecuteInEditor { get; }
+        [SerializeField] private AudioSource _audioSource;
+        
+        
+        public override void OnVFXOutputEvent(VFXEventAttribute eventAttribute)
+        {
+            if (_audioSource != null)
+            {
+                _audioSource.Play();
+            }
+        }
+
+        
+    }
+}
