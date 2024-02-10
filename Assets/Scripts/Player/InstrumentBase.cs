c170f790ef649dcd668854669ec221feeeead98a
Hamid Bashiri <hvbashiri@gmail.com>
2024-02-10 18:41:23 +0330

    fix: fixed ball message position to not rotate with ball movement;

--- Assets/Scripts/Player/InstrumentBase.cs
+++ Assets/Scripts/Player/InstrumentBase.cs
@@ -1,9 +1,45 @@
-﻿using UnityEngine;
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 namespace Player
 {
     public class InstrumentBase : MonoBehaviour
     {
-        
+        [SerializeField] private string message;
+        [SerializeField] private GameObject panelPrefab;
+        [SerializeField] private Vector3 panelPositionOffset;
+
+        private XRGrabInteractable _grableInteractable;
+        private GameObject _messagePanel;
+
+        private void Awake()
+        {
+            _grableInteractable = GetComponent<XRGrabInteractable>();
+            _grableInteractable.firstSelectEntered.AddListener( _ => _messagePanel.SetActive(false));
+            _grableInteractable.lastSelectExited.AddListener(SetMessagePanel);
+            _grableInteractable.activated.AddListener(_ => _grableInteractable.movementType = XRBaseInteractable.MovementType.Instantaneous);
+            _grableInteractable.deactivated.AddListener(_ => _grableInteractable.movementType = XRBaseInteractable.MovementType.VelocityTracking);
+        }
+
+        private void SetMessagePanel(SelectExitEventArgs eventData)
+        {
+            var playerPosition = MainPlayer.Instance.transform.position;
+            _messagePanel.transform.rotation = Quaternion.LookRotation(new Vector3(playerPosition.x,
+                transform.position.y, playerPosition.z) - transform.position, Vector3.up);
+            _messagePanel.SetActive(true);
+        }
+
+        private void Start()
+        {
+            _messagePanel = Instantiate(panelPrefab, transform.position + panelPositionOffset, Quaternion.identity);
+            _messagePanel.GetComponentInChildren<TextMeshPro>().text = message;
+        }
+
+        private void Update()
+        {
+            _messagePanel.transform.position = transform.position + panelPositionOffset;
+        }
     }
 }
